1371634eaba8b2811948ac6f168c24a23d9461a4
EstuadianteBarandiaran <91648871+EstuadianteBarandiaran@users.noreply.github.com>
2021-11-04 22:21:44 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>02 Barandiaran Jonathan</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="Estilos_Boton.css">
+
+</head>
+<body>
+	<header>    
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo1.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Inicio</a></li>
+					<li><a href="Medicamento.html">Medicamento</a></li>
+					<li><a href="Empleado.html">Empleado</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
